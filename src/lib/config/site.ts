9e99d10cb4cc2fb1db952144c19a51a67b1c17e3
LavaWaffle <bjxobjxo@gmail.com>
2024-07-22 17:07:35 -0700

    customized profile

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,27 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'Hazel',
+  subtitle: 'Engineering Blog Portfolio',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: 'Engineering Blog Portfolio',
   author: {
     avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    name: 'Hazel',
+    status: '',
+    metadata: [
+      {
+        text: '',
+        icon: 'i-simple-icons-github',
+        link: 'https://github.com/LavaWaffle'
+      },
+      {
+        text: '',
+        icon: 'i-simple-icons-maildotru',
+        link: 'mailto:parab3@illinois.edu'
+      }
+    ],
+    bio: 'Aspiring Computer Engineer at the University of Illinois at Urbana-Champaign.'
   },
   themeColor: '#3D4451'
 }
